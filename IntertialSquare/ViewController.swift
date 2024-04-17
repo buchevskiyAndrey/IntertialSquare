2843a7d297ceb629ed3650f790610cde07474bc6
Andrey Buchevskiy <buchevskiy@icloud.com>
2024-04-17 12:19:42 +0300

    Intertial Square

--- IntertialSquare/ViewController.swift
+++ IntertialSquare/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+
+    lazy var cardView: UIView = {
+        let view = UIView()
+        view.frame.size = CGSize(width: 100, height: 100)
+        view.center = self.view.center
+        view.backgroundColor = .blue
+        view.layer.cornerRadius = 20
+        return view
+    }()
+
+    lazy var animator = UIDynamicAnimator(referenceView: view)
+    var snapping: UISnapBehavior!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .white
+        view.addSubview(cardView)
+
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        view.addGestureRecognizer(recognizer)
     }
 
+    @objc
+    func handleTap(sender: UITapGestureRecognizer) {
+        let tapPoint = sender.location(in: view)
 
+        if snapping != nil {
+            animator.removeBehavior(snapping)
+        }
+
+        snapping = UISnapBehavior(item: cardView, snapTo: tapPoint)
+        animator.addBehavior(snapping)
+    }
 }
-
